c9cd357ea1c7fbee8222b48e749fadf98d4f9173
Yashi Saini <yashisaini718@gmail.com>
2026-06-19 22:34:33 +0530

    added env_example.txt

--- app/config.py
+++ app/config.py
@@ -6,10 +6,17 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True
+    }
+
     SECRET_KEY=os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
+
     JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=7)
+    
     JWT_TOKEN_LOCATION=["headers"]
     JWT_HEADER_TYPE="Bearer"
     JWT_BLACKLIST_ENABLED=True
